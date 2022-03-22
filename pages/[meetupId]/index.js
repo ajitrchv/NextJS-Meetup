76994422b0f0929560318ae69b837352199427f9
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-03-22 11:32:55 +0530

    cloud fetch and insert donr succesfully, with MongoDB client

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,53 +1,63 @@
 //import { Fragment } from "react/cjs/react.production.min";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Amber_palace%2C_Jaipur.jpg/1024px-Amber_palace%2C_Jaipur.jpg"
-      title="Rajastan Meetup"
-      address="Jaipur, Rajastan, India"
-      description="Rajasthan is a state in northern India. It covers 342,239 square kilometres or 10.4 percent of Indias total geographical area. It is the largest Indian state by area and the seventh largest by population."
+      image= {props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://hedreich:bqMqgjPjeLBVX3Sq@cluster0.fbx9r.mongodb.net/hedreich?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray(); //only fetch the ids, first{} is filters, and the second one is which fields to select.
+  console.log("=========Meetups===========");
+  console.log(meetups);
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
+    paths: meetups.map((meetup) => ({
+      params: {
+        meetupId: meetup._id.toString(),
       },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-      {
-        params: {
-          meetupId: "m3",
-        },
-      },
-    ],
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
   //fetch data here
+  const client = await MongoClient.connect(
+    "mongodb+srv://hedreich:bqMqgjPjeLBVX3Sq@cluster0.fbx9r.mongodb.net/hedreich?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({_id: ObjectId(meetupId)},{}); //only fetch the ids, first{} is filters, and the second {} is which fields to select.
+  
+  client.close();
+  
   return {
     props: {
       meetupData: {
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Amber_palace%2C_Jaipur.jpg/1024px-Amber_palace%2C_Jaipur.jpg",
-        id: meetupId,
-        title: "Rajastan Meetup",
-        address: "Jaipur, Rajastan, India",
-        description:
-          "Rajasthan is a state in northern India. It covers 342,239 square kilometres or 10.4 percent of Indias total geographical area. It is the largest Indian state by area and the seventh largest by population.",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
